a05fc211b19143de3cdd995a8ef27039444e846c
Sacha <sacha.mong-hune@epitech.eu>
2025-11-19 14:13:20 +0400

    [TEST] build server

--- build.bat
+++ build.bat
@@ -1,9 +1,15 @@
+rem 1. Crée le dossier build
+mkdir build
+
+rem 2. Détecte le profil Conan
 conan profile detect
 
-conan install . --build=missing
+rem 3. Installe les dépendances avec Conan
+conan install . --build=missing -if build
 
-dir build\generators
+rem 4. Génère le projet avec CMake
+cd build
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release
 
-conan install . --build=missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
-
-cmake --build build --config Release
+rem 5. Compile le projet
+cmake --build . --config Release
