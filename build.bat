47d92d2e5d464ae2fa2472a82e4c8e7cd1e75e98
Sacha <sacha.mong-hune@epitech.eu>
2025-11-19 14:25:30 +0400

    [TEST] build server

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rem 2. Détecte le profil Conan (ignore si déjà existant)
 conan profile detect
 
 rem 3. Installe les dépendances avec Conan dans build
-conan install . --build=missing -if build
+conan install . --build=missing
 
 rem 4. Génère le projet avec CMake en utilisant le toolchain Conan
 cd build
