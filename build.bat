4f67e9c9c2496bc77da0445292ab7e661070e0cf
Sacha <sacha.mong-hune@epitech.eu>
2025-11-19 13:58:02 +0400

    [TEST] build server

--- build.bat
+++ build.bat
@@ -2,9 +2,6 @@ conan profile detect
 
 conan install . --build=missing
 
-cmake -S . -B ./build ^
-    -G "Visual Studio 17 2022" ^
-    -DCMAKE_TOOLCHAIN_FILE=build\generators\conan_toolchain.cmake ^
-    -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=build\generators\conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build --config Release
