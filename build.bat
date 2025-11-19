2c8ea893992e7aa5b70ce04e6edeb55bfb89ef81
Sacha <sacha.mong-hune@epitech.eu>
2025-11-19 13:23:50 +0400

    [TEST] build server

--- build.bat
+++ build.bat
@@ -1,10 +1,5 @@
-conan install . --build=missing \
-    -s compiler.libcxx=libstdc++11 \
-    -c tools.system.package_manager:mode=install \
-    -c tools.system.package_manager:sudo=True
+conan install . --build=missing
 
-cmake -S . -B build \
-    -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake \
-    -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build --config Release
