469b8560f719acda05eb21ecaf08e13dea8b628f
Sacha <sacha.mong-hune@epitech.eu>
2025-11-19 14:35:55 +0400

    [TEST] build server

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
+rem Crée le dossier build si absent
 If not exist build\ (mkdir build)
+
+rem Installe les dépendances avec Conan dans build
 cd build
-conan install .. --build=missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
-cmake -B ./build -DCMAKE_BUILD_TYPE=Release
+
+rem Génère le projet avec CMake en utilisant le toolchain Conan et CMP0091=NEW
+cmake -B ./build -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/generators/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
+
+rem Compile le projet
 cmake --build ./build -j
