d53fec6c49b3eca680dc97ee05e909c7ab4704e8
Sacha <sacha.mong-hune@epitech.eu>
2026-01-14 10:53:58 +0400

    [MODIF] build.bat, add rtype_server

--- build.bat
+++ build.bat
@@ -12,4 +12,4 @@ rem Génère le projet avec CMake en utilisant le toolchain Conan et CMP0091=NEW
 cmake -B ./build -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/generators/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 
 rem Compile le projet
-cmake --build ./build --config Release --target rtype_client
+cmake --build ./build --config Release --target rtype_client rtype_server
